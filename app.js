da0da8671b8366cf0bbb9ab50bc1bf75e5411f8a
Margarita Gicheva <margoshka_love20@mail.ru>
2015-07-16 20:36:13 +0300

    play pause stop

--- app.js
+++ app.js
@@ -1,15 +1,1 @@
-var color, i, j, k, len, ref, ref1, sides, x;
 
-eval($.turtle());
-
-speed(100);
-
-
-    for (x = k = 1, ref1 = sides; 1 <= ref1 ? k <= ref1 : k >= ref1; x = 1 <= ref1 ? ++k : --k) {
-      fd(100 / sides);
-      lt(360 / sides);
-    }
-    pen('red');
-    fd(40);
-
-  slide(40, -160);
